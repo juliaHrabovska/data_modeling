b6b3a5611293a4ad6ee92626c2fda51acaf5cda2
Viacheslav Bobrov <viacheslav_bobrov@epam.com>
2017-10-07 15:46:06 +0300

    Initial Project Import

---
+++ DataModeling/DataModeling/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataModeling
+{
+    public partial class Form1 : Form
+    {
+        private Module1 module1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(module1 == null)
+            {
+                module1 = new Module1();
+                module1.Show();
+            }
+            else
+            {
+                module1.Focus();
+            }
+            
+        }
+    }
+}
---
+++ DataModeling/DataModeling/Module1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataModeling
+{
+    public partial class Module1 : Form
+    {
+        public Module1()
+        {
+            InitializeComponent();
+        }
+    }
+}
